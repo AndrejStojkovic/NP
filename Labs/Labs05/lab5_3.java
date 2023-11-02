5af4a94658090fcbee296cb02eb1951c64739e53
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2023-11-02 23:38:32 +0100

    Added Labs 5

---
+++ Labs/Labs05/lab5_3.java
@@ -0,0 +1,238 @@
+// Lab 5.3
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class EmptyQueueException extends Exception {
+    EmptyQueueException() { }
+}
+
+class Node<T> {
+    private T element;
+    private Node<T> next;
+
+    Node() {
+        this.element = null;
+        this.next = null;
+    }
+
+    Node(T element, Node<T> next) {
+        this.element = element;
+        this.next = next;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        if(next != null) {
+            this.next = next;
+        }
+    }
+}
+
+class Queue<T> {
+    private Node<T> first;
+    private Node<T> last;
+
+    Queue() {
+        first = null;
+        last = null;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void enqueue(T element) {
+        if(first == null) {
+            first = new Node<T>(element, null);
+            last = first;
+        } else {
+            last.setNext(new Node<T>(element, null));
+            last = last.getNext();
+        }
+    }
+
+    public T dequeue() throws EmptyQueueException {
+        if(isEmpty()) {
+            throw new EmptyQueueException();
+        }
+        Node<T> element = first;
+        first = first.getNext();
+        if(first == null) {
+            last = null;
+        }
+        return element.getElement();
+    }
+
+    public T peek() throws EmptyQueueException {
+        if(first == null) {
+            throw new EmptyQueueException();
+        }
+        return first.getElement();
+    }
+
+    public T inspect() throws EmptyQueueException {
+        if(first == null) {
+            throw new EmptyQueueException();
+        }
+        return last.getElement();
+    }
+
+    public int count() {
+        if(first == null) {
+            return 0;
+        }
+
+        int ct = 0;
+        Node<T> curr = first;
+        while(curr != null) {
+            curr = curr.getNext();
+            ct++;
+        }
+        return ct;
+    }
+}
+
+public class QueueTest {
+    public static void main(String[] args) throws EmptyQueueException {
+        Scanner jin = new Scanner(System.in);
+        int k = jin.nextInt();
+        if ( k == 0 ) { //Simple test case with one int element
+            int t = jin.nextInt();
+            Queue<Integer> queue = new Queue<Integer>();
+            System.out.println("Queue empty? - "+queue.isEmpty());
+            System.out.println("Queue count? - "+queue.count());
+            System.out.println("Queue enqueue "+t);
+            queue.enqueue(t);
+            System.out.println("Queue empty? - "+queue.isEmpty());
+            System.out.println("Queue count? - "+queue.count());
+            System.out.println("Queue dequeue? - "+queue.dequeue());
+            System.out.println("Queue empty? - "+queue.isEmpty());
+            System.out.println("Queue count? - "+queue.count());
+        }
+        if ( k == 1 ) { //a more complex test with strings
+            Queue<String> queue = new Queue<String>();
+            int counter = 0;
+            while ( jin.hasNextInt() ) {
+                String t = jin.next();
+                queue.enqueue(t);
+                ++counter;
+            }
+            for ( int i = 0 ; i < counter ; ++i ) {
+                System.out.println(queue.dequeue());
+            }
+            queue.enqueue(jin.next());
+            System.out.println("Queue inspect? - "+queue.inspect());
+            System.out.println("Queue peek? - "+queue.peek());
+            queue.enqueue(queue.dequeue());
+            queue.enqueue(jin.next());
+            System.out.println("Queue inspect? - "+queue.inspect());
+            System.out.println("Queue peek? - "+queue.peek());
+        }
+        if ( k == 2 ) {
+            Queue<String> queue = new Queue<String>();
+            String next = "";
+            int counter = 0;
+            while ( true ) {
+                next = jin.next();
+                if ( next.equals("stop") ) break;
+                queue.enqueue(next);
+                ++counter;
+            }
+            while ( !queue.isEmpty() ) {
+                if ( queue.count()<counter) System.out.print(" ");
+                System.out.print(queue.dequeue());
+            }
+        }
+        if ( k == 3 ) { //random testing
+            Queue<Double> queue = new Queue<Double>();
+            LinkedList<Double> java_queue = new LinkedList<Double>();
+            boolean flag = true;
+            int n = jin.nextInt();
+            for ( int i = 0 ; i < n ; ++i ) {
+                double q = Math.random();
+                if ( q < 0.5 ) {
+                    double t = Math.random();
+                    queue.enqueue(t);
+                    java_queue.addFirst(t);
+                }
+                if ( q < 0.8 && q >= 0.5 ) {
+                    if ( ! java_queue.isEmpty() ) {
+                        double t1 = java_queue.removeLast();
+                        double t2 = queue.dequeue();
+                        flag &= t1==t2;
+                    }
+                    else {
+                        flag &= java_queue.isEmpty()==queue.isEmpty();
+                    }
+                }
+                if ( q < 0.9 && q >= 0.8 ) {
+                    if ( ! java_queue.isEmpty() ) {
+                        double t1 = java_queue.peekLast();
+                        double t2 = queue.peek();
+                        flag &= t1==t2;
+                    }
+                    else {
+                        flag &= java_queue.isEmpty()==queue.isEmpty();
+                    }
+                }
+                if ( q < 1 && q >= 0.9 ) {
+                    if ( ! java_queue.isEmpty() ) {
+                        double t1 = java_queue.peekFirst();
+                        double t2 = queue.inspect();
+                        flag &= t1==t2;
+                    }
+                    else {
+                        flag &= java_queue.isEmpty()==queue.isEmpty();
+                    }
+                }
+                flag &= java_queue.size()==queue.count();
+            }
+            System.out.println("Compared to the control queue the results were the same? - "+flag);
+        }
+        if ( k == 4 ) { //performance testing
+            Queue<Double> queue = new Queue<Double>();
+            int n = jin.nextInt();
+            for ( int i = 0 ; i < n ; ++i ) {
+                if ( Math.random() < 0.5 ) {
+                    queue.enqueue(Math.random());
+                }
+                else {
+                    if ( ! queue.isEmpty() ) {
+                        queue.dequeue();
+                    }
+                }
+            }
+            System.out.println("You implementation finished in less then 3 seconds, well done!");
+        }
+        if ( k == 5 ) { //Exceptions testing
+            Queue<String> queue = new Queue<String>();
+            try {
+                queue.dequeue();
+            }
+            catch ( Exception e ) {
+                System.out.println(e.getClass().getSimpleName());
+            }
+            try {
+                queue.peek();
+            }
+            catch ( Exception e ) {
+                System.out.println(e.getClass().getSimpleName());
+            }
+            try {
+                queue.inspect();
+            }
+            catch ( Exception e ) {
+                System.out.println(e.getClass().getSimpleName());
+            }
+        }
+    }
+
+}
---
+++ Labs/Labs05/lab5_4.java
@@ -0,0 +1,96 @@
+// Lab 5.4
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class ComplexNumber<T extends Number, U extends Number> implements Comparable<ComplexNumber<?, ?>> {
+    private T real;
+    private U imaginary;
+
+    ComplexNumber() {
+        this.real = null;
+        this.imaginary = null;
+    }
+
+    ComplexNumber(T real, U imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public T getReal() {
+        return real;
+    }
+
+    public U getImaginary() {
+        return imaginary;
+    }
+
+    public double modul() {
+        return Math.sqrt(Math.pow(real.doubleValue(), 2) + Math.pow(imaginary.doubleValue(), 2));
+    }
+
+    public int compareTo(ComplexNumber<?, ?> o) {
+        return Double.compare(this.modul(), o.modul());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%.2f%+.2fi", real.doubleValue(), imaginary.doubleValue());
+    }
+}
+
+public class ComplexNumberTest {
+
+    public static void main(String[] args) {
+        Scanner jin = new Scanner(System.in);
+        int k = jin.nextInt();
+        if ( k == 0 ) { //test simple functions int
+            int r = jin.nextInt();int i = jin.nextInt();
+            ComplexNumber<Integer, Integer> c = new ComplexNumber<Integer, Integer>(r, i);
+            System.out.println(c);
+            System.out.println(c.getReal());
+            System.out.println(c.getImaginary());
+            System.out.println(c.modul());
+        }
+        if ( k == 1 ) { //test simple functions float
+            float r = jin.nextFloat();
+            float i = jin.nextFloat();
+            ComplexNumber<Float, Float> c = new ComplexNumber<Float, Float>(r, i);
+            System.out.println(c);
+            System.out.println(c.getReal());
+            System.out.println(c.getImaginary());
+            System.out.println(c.modul());
+        }
+        if ( k == 2 ) { //compareTo int
+            LinkedList<ComplexNumber<Integer,Integer>> complex = new LinkedList<ComplexNumber<Integer,Integer>>();
+            while ( jin.hasNextInt() ) {
+                int r = jin.nextInt(); int i = jin.nextInt();
+                complex.add(new ComplexNumber<Integer, Integer>(r, i));
+            }
+            System.out.println(complex);
+            Collections.sort(complex);
+            System.out.println(complex);
+        }
+        if ( k == 3 ) { //compareTo double
+            LinkedList<ComplexNumber<Double,Double>> complex = new LinkedList<ComplexNumber<Double,Double>>();
+            while ( jin.hasNextDouble() ) {
+                double r = jin.nextDouble(); double i = jin.nextDouble();
+                complex.add(new ComplexNumber<Double, Double>(r, i));
+            }
+            System.out.println(complex);
+            Collections.sort(complex);
+            System.out.println(complex);
+        }
+        if ( k == 4 ) { //compareTo mixed
+            LinkedList<ComplexNumber<Double,Integer>> complex = new LinkedList<ComplexNumber<Double,Integer>>();
+            while ( jin.hasNextDouble() ) {
+                double r = jin.nextDouble(); int i = jin.nextInt();
+                complex.add(new ComplexNumber<Double, Integer>(r, i));
+            }
+            System.out.println(complex);
+            Collections.sort(complex);
+            System.out.println(complex);
+        }
+    }
+}
