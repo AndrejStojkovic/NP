29eadc5c30fa29de35a0091dfeb9398593bd722d
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2023-11-08 20:33:36 +0100

    Added K1 2 and 3

---
+++ Exercises/K1/k1_2.java
@@ -0,0 +1,200 @@
+// K1 2
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.*;
+
+class IrregularCanvasException extends Exception {
+    IrregularCanvasException(String id, double maxArea) {
+        super(String.format("Canvas %s has a shape with area larger than %.2f", id, maxArea));
+    }
+}
+
+enum Type {
+    Square,
+    Circle
+}
+
+class Shape implements Comparable<Shape> {
+    protected Type type;
+    protected int size;
+
+    Shape(Type type, int size) {
+        this.type = type;
+        this.size = size;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Shape o) {
+        return Double.compare(getArea(), o.getArea());
+    }
+}
+
+class Square extends Shape {
+    public Square(int size) {
+        super(Type.Square, size);
+    }
+
+    @Override
+    public double getArea() {
+        return size * size;
+    }
+}
+
+class Circle extends Shape {
+    public Circle(int size) {
+        super(Type.Circle, size);
+    }
+
+    @Override
+    public double getArea() {
+        return size * size * Math.PI;
+    }
+}
+
+class Canvas implements Comparable<Canvas> {
+    private String canvasId;
+    private ArrayList<Shape> shapes;
+
+    Canvas() {
+        canvasId = "";
+        shapes = new ArrayList<>();
+    }
+
+    Canvas(String canvasId, ArrayList<Shape> shapes) {
+        this.canvasId = canvasId;
+        this.shapes = shapes;
+    }
+
+    public String getCanvasId() {
+        return canvasId;
+    }
+
+    public double minArea() {
+        return Collections.min(shapes).getArea();
+    }
+
+    public double maxArea() {
+        return Collections.max(shapes).getArea();
+    }
+
+    public double averageArea() {
+        double sum = 0;
+        for(Shape shape : shapes) {
+            sum += shape.getArea();
+        }
+        return sum / shapes.size();
+    }
+
+    public double getTotalArea() {
+        double sum = 0;
+        for(Shape shape : shapes) {
+            sum += shape.getArea();
+        }
+        return sum;
+    }
+
+    public long getSquares() {
+        return shapes.stream().filter(x -> x.getType().equals(Type.Square)).count();
+    }
+
+    public long getCircles() {
+        return shapes.stream().filter(x -> x.getType().equals(Type.Circle)).count();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d %d %d %.2f %.2f %.2f", canvasId, shapes.size(), getCircles(), getSquares(), minArea(), maxArea(), averageArea());
+    }
+
+    @Override
+    public int compareTo(Canvas o) {
+        return Double.compare(getTotalArea(), o.getTotalArea());
+    }
+}
+
+class ShapesApplication {
+    private ArrayList<Canvas> list;
+    private double maxArea;
+    private
+
+    ShapesApplication() {
+        list = new ArrayList<>();
+        maxArea = 0;
+    }
+
+    ShapesApplication(double maxArea) {
+        this.list = new ArrayList<>();
+        this.maxArea = maxArea;
+    }
+
+    public void addCanvas(Canvas c) throws IrregularCanvasException {
+        if(c.maxArea() > maxArea) {
+            throw new IrregularCanvasException(c.getCanvasId(), maxArea);
+        }
+        list.add(c);
+    }
+
+    public void readCanvases(InputStream inputStream) {
+        Scanner input = new Scanner(inputStream);
+
+        while(input.hasNextLine()) {
+            String canvas = input.nextLine();
+
+            String canvasId = canvas.split(" ")[0];
+            String [] str = canvas.split(" ");
+
+            ArrayList<Shape> shapes = new ArrayList<>();
+
+            for(int i = 1; i < str.length; i += 2) {
+                int value = Integer.parseInt(str[i + 1]);
+
+                if(Objects.equals(str[i], "C")) {
+                    shapes.add(new Circle(value));
+                } else if(Objects.equals(str[i], "S")) {
+                    shapes.add(new Square(value));
+                }
+            }
+
+            try {
+                addCanvas(new Canvas(canvasId, shapes));
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void printCanvases(OutputStream outputStream) {
+        PrintWriter pw = new PrintWriter(outputStream);
+
+        ArrayList<Canvas> sorted = list;
+        sorted.sort(Collections.reverseOrder());
+
+        for(Canvas c : list) {
+            pw.println(c);
+        }
+
+        pw.flush();
+    }
+}
+
+public class Shapes2Test {
+    public static void main(String[] args) {
+        ShapesApplication shapesApplication = new ShapesApplication(10000);
+
+        System.out.println("===READING CANVASES AND SHAPES FROM INPUT STREAM===");
+        shapesApplication.readCanvases(System.in);
+
+        System.out.println("===PRINTING SORTED CANVASES TO OUTPUT STREAM===");
+        shapesApplication.printCanvases(System.out);
+    }
+}
---
+++ Exercises/K1/k1_3.java
@@ -0,0 +1,209 @@
+// K1 3
+
+import java.util.*;
+
+interface IFile extends Comparable<IFile> {
+    String getFileName();
+    long getFileSize();
+    String getFileInfo(int sub);
+    void sortBySize();
+    long findLargestFile();
+
+    @Override
+    default int compareTo(IFile o) {
+        return Long.compare(getFileSize(), o.getFileSize());
+    }
+}
+
+class FileNameExistsException extends Exception {
+    FileNameExistsException(String file, String folder) {
+        super(String.format("There is already a file named %s in the folder %s", file, folder));
+    }
+}
+
+class File implements IFile {
+    String name;
+    long size;
+
+    File() {
+        this.name = "Untitled";
+        this.size = 0;
+    }
+
+    File(String name, long size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    @Override
+    public String getFileName() {
+        return name;
+    }
+
+    @Override
+    public long getFileSize() {
+        return size;
+    }
+
+    @Override
+    public String getFileInfo(int sub) {
+        return " ".repeat(Math.max(0, 4 * sub)) + String.format("File name: %10s File size: %10d\n", name, size);
+    }
+
+    @Override
+    public void sortBySize() { }
+
+    @Override
+    public long findLargestFile() {
+        return size;
+    }
+}
+
+class Folder implements IFile {
+    String name;
+    long size;
+    ArrayList<IFile> list;
+
+    Folder() {
+        this.name = "Untitled";
+        this.size = 0;
+        this.list = new ArrayList<>();
+    }
+
+    Folder(String name) {
+        this.name = name;
+        this.size = 0;
+        this.list = new ArrayList<>();
+    }
+
+    public void addFile(IFile file) throws FileNameExistsException {
+        String fileName = file.getFileName();
+        for(IFile f : list) {
+            if(Objects.equals(fileName, f.getFileName())) {
+                throw new FileNameExistsException(fileName, name);
+            }
+        }
+
+        list.add(file);
+        size += file.getFileSize();
+    }
+
+    @Override
+    public String getFileName() {
+        return name;
+    }
+
+    @Override
+    public long getFileSize() {
+        return size;
+    }
+
+    @Override
+    public String getFileInfo(int sub) {
+        StringBuilder str = new StringBuilder();
+        str.append(" ".repeat(Math.max(0, 4 * sub)));
+        str.append(String.format("Folder name: %10s Folder size: %10d\n", name, size));
+        for(IFile f : list) { str.append(f.getFileInfo(sub + 1)); }
+        return str.toString();
+    }
+
+    @Override
+    public void sortBySize() {
+        list.sort(IFile::compareTo);
+
+        for(IFile f : list) {
+            f.sortBySize();
+        }
+    }
+
+    @Override
+    public long findLargestFile() {
+        return list.stream().mapToLong(IFile::findLargestFile).max().orElse(0);
+    }
+}
+
+class FileSystem {
+    Folder dir;
+
+    FileSystem() {
+        dir = new Folder("root");
+    }
+
+    public void addFile(IFile file) throws FileNameExistsException {
+        dir.addFile(file);
+    }
+
+    public long findLargestFile() {
+        return dir.findLargestFile();
+    }
+
+    public void sortBySize() {
+        dir.sortBySize();;
+    }
+
+    @Override
+    public String toString() {
+        return dir.getFileInfo(0);
+    }
+}
+
+public class FileSystemTest {
+
+    public static Folder readFolder (Scanner sc)  {
+
+        Folder folder = new Folder(sc.nextLine());
+        int totalFiles = Integer.parseInt(sc.nextLine());
+
+        for (int i=0;i<totalFiles;i++) {
+            String line = sc.nextLine();
+
+            if (line.startsWith("0")) {
+                String fileInfo = sc.nextLine();
+                String [] parts = fileInfo.split("\\s+");
+                try {
+                    folder.addFile(new File(parts[0], Long.parseLong(parts[1])));
+                } catch (FileNameExistsException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            else {
+                try {
+                    folder.addFile(readFolder(sc));
+                } catch (FileNameExistsException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+
+        return folder;
+    }
+
+    public static void main(String[] args)  {
+
+        //file reading from input
+
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("===READING FILES FROM INPUT===");
+        FileSystem fileSystem = new FileSystem();
+        try {
+            fileSystem.addFile(readFolder(sc));
+        } catch (FileNameExistsException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("===PRINTING FILE SYSTEM INFO===");
+        System.out.println(fileSystem.toString());
+
+        System.out.println("===PRINTING FILE SYSTEM INFO AFTER SORTING===");
+        fileSystem.sortBySize();
+        System.out.println(fileSystem.toString());
+
+        System.out.println("===PRINTING THE SIZE OF THE LARGEST FILE IN THE FILE SYSTEM===");
+        System.out.println(fileSystem.findLargestFile());
+
+
+
+
+    }
+}
