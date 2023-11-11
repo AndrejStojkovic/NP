95d3b386b86a1cf7183a24b7597601bf49b8019c
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2023-11-11 20:56:47 +0100

    Added more K1 exercises

---
+++ Exercises/K1/k1_15.java
@@ -0,0 +1,149 @@
+// K1 15
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class AmountNotAllowedException extends Exception {
+    AmountNotAllowedException(int sum) {
+        super(String.format("Receipt with amount %d is not allowed to be scanned", sum));
+    }
+}
+
+class Item {
+    String type;
+    int price;
+
+    Item() {
+        type = "A";
+        price = 0;
+    }
+
+    Item(String type, int price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public double getTax() {
+        if(type.equals("A")) {
+            return price * 0.18 * 0.15;
+        } else if(type.equals("B")) {
+            return price * 0.05 * 0.15;
+        }
+        return 0;
+    }
+}
+
+class Fiskalna {
+    String id;
+    ArrayList<Item> arr;
+
+    Fiskalna() {
+        id = "None";
+        arr = new ArrayList<>();
+    }
+
+    Fiskalna(String id, ArrayList<Item> arr) {
+        this.id = id;
+        this.arr = arr;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void addItem(Item item) {
+        arr.add(item);
+    }
+
+    public void check() throws AmountNotAllowedException {
+        if(getTotalPrice() >= 30000) {
+            throw new AmountNotAllowedException(getTotalPrice());
+        }
+    }
+
+    public int getTotalPrice() {
+        int s = 0;
+        for(Item item : arr) s += item.getPrice();
+        return s;
+    }
+
+    public double getTotalTax() {
+        double s = 0;
+        for(Item item : arr) s += item.getTax();
+        return s;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d %.2f", id, getTotalPrice(), getTotalTax());
+    }
+}
+
+class MojDDV {
+    ArrayList<Fiskalna> list;
+
+    MojDDV() {
+        list = new ArrayList<>();
+    }
+
+    public void readRecords(InputStream in) {
+        Scanner input = new Scanner(in);
+
+        while(input.hasNextLine()) {
+            String lineInput = input.nextLine();
+            String [] line = lineInput.split(" ");
+
+            Fiskalna f = new Fiskalna();
+            f.setId(line[0]);
+
+            for(int i = 1; i < line.length; i += 2) {
+                f.addItem(new Item(line[i + 1], Integer.parseInt(line[i])));
+            }
+
+            try {
+                f.check();
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+
+            list.add(f);
+        }
+    }
+
+    public void printTaxReturns(OutputStream out) {
+        PrintWriter pw = new PrintWriter(out);
+        for(Fiskalna f : list) pw.println(f);
+        pw.flush();
+    }
+}
+
+public class MojDDVTest {
+
+    public static void main(String[] args) {
+
+        MojDDV mojDDV = new MojDDV();
+
+        System.out.println("===READING RECORDS FROM INPUT STREAM===");
+        mojDDV.readRecords(System.in);
+
+        System.out.println("===PRINTING TAX RETURNS RECORDS TO OUTPUT STREAM ===");
+        mojDDV.printTaxReturns(System.out);
+
+    }
+}
---
+++ Exercises/K1/k1_17.java
@@ -0,0 +1,215 @@
+// K1 17
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+class CategoryNotFoundException extends Exception {
+    CategoryNotFoundException(String name) {
+        super(String.format("Category %s was not found", name));
+    }
+}
+
+class Category {
+    String name;
+
+    Category() {
+        name = "None";
+    }
+
+    Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+abstract class NewsItem {
+    protected String title;
+    protected Date date;
+    protected Category category;
+
+    NewsItem() {
+        title = "News";
+        date = new Date();
+        category = new Category();
+    }
+
+    NewsItem(String title, Date date, Category category) {
+        this.title = title;
+        this.date = date;
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public String getTeaser() {
+        return "Coming soon!";
+    }
+}
+
+class TextNewsItem extends NewsItem {
+    String text;
+
+    TextNewsItem() {
+        super();
+        text = "text";
+    }
+
+    TextNewsItem(String title, Date date, Category category, String text) {
+        super(title, date, category);
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String getTeaser() {
+        long duration = Calendar.getInstance().getTime().getTime() - date.getTime();
+        return String.format("%s\n%d\n%s", title, TimeUnit.MILLISECONDS.toMinutes(duration), text.length() < 80 ? text : text.substring(0, 80));
+    }
+}
+
+class MediaNewsItem extends NewsItem {
+    String url;
+    int views;
+
+    MediaNewsItem() {
+        url = "www.finki.ukim.mk";
+        views = 0;
+    }
+
+    MediaNewsItem(String title, Date date, Category category, String url, int views) {
+        super(title, date, category);
+        this.url = url;
+        this.views = views;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    @Override
+    public String getTeaser() {
+        long duration = Calendar.getInstance().getTime().getTime() - date.getTime();
+        return String.format("%s\n%d\n%s\n%d", title, TimeUnit.MILLISECONDS.toMinutes(duration), url, views);
+    }
+}
+
+class FrontPage {
+    Category [] categories;
+    ArrayList<NewsItem> list;
+
+    FrontPage(Category [] categories) {
+        this.categories = Arrays.copyOf(categories, categories.length);
+        this.list = new ArrayList<>();
+    }
+
+    public void addNewsItem(NewsItem newsItem) {
+        list.add(newsItem);
+    }
+
+    public List<NewsItem> listByCategory(Category category) {
+        return list.stream().filter(i -> i.getCategory().equals(category)).collect(Collectors.toList());
+    }
+
+    public List<NewsItem> listByCategoryName(String category) throws CategoryNotFoundException {
+        for (Category c : categories) {
+            if (c.getName().equals(category)) {
+                return list.stream().filter(i -> i.getCategory().getName().equals(category)).collect(Collectors.toList());
+            }
+        }
+
+        throw new CategoryNotFoundException(category);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        for(NewsItem item : list) {
+            str.append(item.getTeaser()).append("\n");
+        }
+        return str.toString();
+    }
+}
+
+public class FrontPageTest {
+    public static void main(String[] args) {
+        // Reading
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        String[] parts = line.split(" ");
+        Category[] categories = new Category[parts.length];
+        for (int i = 0; i < categories.length; ++i) {
+            categories[i] = new Category(parts[i]);
+        }
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        FrontPage frontPage = new FrontPage(categories);
+        Calendar cal = Calendar.getInstance();
+        for (int i = 0; i < n; ++i) {
+            String title = scanner.nextLine();
+            cal = Calendar.getInstance();
+            int min = scanner.nextInt();
+            cal.add(Calendar.MINUTE, -min);
+            Date date = cal.getTime();
+            scanner.nextLine();
+            String text = scanner.nextLine();
+            int categoryIndex = scanner.nextInt();
+            scanner.nextLine();
+            TextNewsItem tni = new TextNewsItem(title, date, categories[categoryIndex], text);
+            frontPage.addNewsItem(tni);
+        }
+
+        n = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < n; ++i) {
+            String title = scanner.nextLine();
+            int min = scanner.nextInt();
+            cal = Calendar.getInstance();
+            cal.add(Calendar.MINUTE, -min);
+            scanner.nextLine();
+            Date date = cal.getTime();
+            String url = scanner.nextLine();
+            int views = scanner.nextInt();
+            scanner.nextLine();
+            int categoryIndex = scanner.nextInt();
+            scanner.nextLine();
+            MediaNewsItem mni = new MediaNewsItem(title, date, categories[categoryIndex], url, views);
+            frontPage.addNewsItem(mni);
+        }
+        // Execution
+        String category = scanner.nextLine();
+        System.out.println(frontPage);
+        for(Category c : categories) {
+            System.out.println(frontPage.listByCategory(c).size());
+        }
+        try {
+            System.out.println(frontPage.listByCategoryName(category).size());
+        } catch(CategoryNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
