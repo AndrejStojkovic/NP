679085f6914c47b5fb0e9b156d9dfe5c49c33b1e
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2024-01-06 19:48:57 +0100

    Added K2 19

---
+++ Exercises/K2/k2_19.java
@@ -0,0 +1,53 @@
+// K2 19
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MapSortingTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        List<String> l = readMapPairs(scanner);
+        if(n==1){
+            Map<String, Integer> map = new HashMap<>();
+            fillStringIntegerMap(l, map);
+            SortedSet<Map.Entry<String, Integer>> s = entriesSortedByValues(map);
+            System.out.println(s);
+        } else {
+            Map<Integer, String> map = new HashMap<>();
+            fillIntegerStringMap(l, map);
+            SortedSet<Map.Entry<Integer, String>> s = entriesSortedByValues(map);
+            System.out.println(s);
+        }
+
+    }
+
+    private static List<String> readMapPairs(Scanner scanner) {
+        String line = scanner.nextLine();
+        String[] entries = line.split("\\s+");
+        return Arrays.asList(entries);
+    }
+
+    static void fillStringIntegerMap(List<String> l, Map<String,Integer> map) {
+        l.stream()
+                .forEach(s -> map.put(s.substring(0, s.indexOf(':')), Integer.parseInt(s.substring(s.indexOf(':') + 1))));
+    }
+
+    static void fillIntegerStringMap(List<String> l, Map<Integer, String> map) {
+        l.stream()
+                .forEach(s -> map.put(Integer.parseInt(s.substring(0, s.indexOf(':'))), s.substring(s.indexOf(':') + 1)));
+    }
+
+    private static <K, V extends  Comparable<? super V>> SortedSet<Map.Entry<K, V>> entriesSortedByValues(Map<K, V> map) {
+        System.out.println(map);
+        SortedSet<Map.Entry<K, V>> set = new TreeSet<>(
+                (entry1, entry2) -> {
+                    int comp = entry2.getValue().compareTo(entry1.getValue());
+                    return comp != 0 ? comp : 1;
+                }
+        );
+        set.addAll(map.entrySet());
+        return set;
+    }
+}
